64a55606ccb7f49d14e67a2191858ec0d5be9473
Simon Lüke <semaphor@resolution1.net>
2024-05-20 07:23:36 +0000

    Daten aktueller Gemeinderat hinzugefügt.

--- clean.R
+++ clean.R
@@ -1,7 +1,15 @@
 library(dplyr)
 
-data <- read.csv('raw_kandidat_innen.csv')
+data <- read.csv('daten/raw_kandidat_innen.csv')
 
 cleaned_data <- data %>% select(-any_of(c('Name')))
 
-write.csv(cleaned_data, 'kandidat_innen.csv')
+write.csv(cleaned_data, 'daten/kandidat_innen-2024-09-06.csv')
+
+
+
+data <- read.csv('daten/raw_aktueller_gemeinderat_2024-03.csv')
+
+cleaned_data <- data %>% select(-any_of(c('Familienname..Vornamen..ggf..zusätzliche.Angaben')))
+
+write.csv(cleaned_data, 'daten/aktueller_gemeinderat_2024-03.csv')
