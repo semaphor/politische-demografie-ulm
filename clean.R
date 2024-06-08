6c234658e48d5dd6fee501a8527eb260d62b03f5
semaphor <semaphor@resolution1.net>
2024-06-08 16:17:48 +0200

    CSV für neuen Gemeinderat schonmal als dummy angelegt. Dateinamen korrigiert.

--- clean.R
+++ clean.R
@@ -5,7 +5,7 @@ data <- read.csv('daten/raw_kandidat_innen.csv')
 
 cleaned_data <- data %>% select(-any_of(c('Lfd...Nr.','Name')))
 
-write.csv(cleaned_data, 'daten/kandidat_innen-2024-09-06.csv', row.names=FALSE)
+write.csv(cleaned_data, 'daten/kandidat_innen-2024-06-09.csv', row.names=FALSE)
 
 
 
@@ -13,4 +13,4 @@ data2 <- read.csv('daten/raw_aktueller_gemeinderat_2024-03.csv')
 
 cleaned_data2 <- data %>% select(-any_of(c('Familienname..Vornamen..ggf..zusätzliche.Angaben')))
 
-write.csv(cleaned_data, 'daten/aktueller_gemeinderat_2024-03.csv')
+write.csv(cleaned_data, 'daten/gemeinderat_2024-03_aktueller.csv')
